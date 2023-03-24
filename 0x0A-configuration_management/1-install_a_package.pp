b6025dfb85c94235ea2c4e47e062ebea3adbddfd
Salvation Makina <salvation.developer@gmail.com>
2023-03-24 17:05:59 +0000

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Install Flask version 2.1.0 from pip3:
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install-flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
+}
+
